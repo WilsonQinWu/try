5d03647b6b0dd076c77610680415e82b258b6340
WilsonQinWu <wuqin_04@hotmail.com>
2019-05-08 08:12:03 -0400

    modified

--- src/beads/BeadTester.java
+++ src/beads/BeadTester.java
@@ -3,22 +3,23 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package beads;
 
 /**
  *
  * @author Megha
  */
-public class BeadTester {
-    public static void main(String[] args)
-    {
-        Beads b1=new Beads();
-        b1.setColor("White");
-        System.out.println(b1.getColor());
-      
-          Beads b2=new Beads("black");
-        System.out.println(b2.getColor());
-          
-    }
+public class BeadTester
+{
+   public static void main (String[] args)
+   {
+      Beads b1 = new Beads();
+      b1.setColor("White");
+      System.out.println(b1.getColor());
+      System.out.println(b1.getLetter());
+
+      Beads b2 = new Beads("black");
+      System.out.println(b2.getColor());
+
+   }
 }
