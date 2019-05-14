a63cf2351d94ae5663bb99f2266516bfe5bbf732
WilsonQinWu <wuqin_04@hotmail.com>
2019-05-14 13:32:11 -0400

    modified

--- src/beads/Beads.java
+++ src/beads/Beads.java
@@ -3,52 +3,63 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package beads;
 
 /**
  *
  * @author Megha,2019
  */
-public class Beads {
+public class Beads
+{
 
-    public String color;
-    public char letter;
-        
-    /**
-     * @param args the command line arguments
-     */
-    Beads(){}
-   Beads(String clr)
+   public String color;
+   public char letter;
+
+   /**
+    * @param args the command line arguments
+    */
+   Beads ()
    {
-       color=clr;
    }
-    /**getter for the color field
-     * @return the color
-     */
-    public String getColor() {
-        return color;
-    }
 
-    /** setter for the color field
-     * @param color the color to set
-     */
-    public void setColor(String clr) {
-        color = clr;
-    }
+   Beads (String clr)
+   {
+      color = clr;
+   }
 
-    /**getter for the letter
-     * @return the letter
-     */
-    public char getLetter() {
-        return letter;
-    }
+   /** getter for the color field
+    *
+    * @return the color
+    */
+   public String getColor ()
+   {
+      return color;
+   }
 
-    /**
-     * @param letter the letter to set
-     */
-    public void setLetter(char ltr) {
-        letter = ltr;
-    }
-    
+   /** setter for the color field
+    *
+    * @param color the color to set
+    */
+   public void setColor (String clr)
+   {
+      color = clr;
+   }
+
+   /** getter for the letter
+    *
+    * @return the letter
+    */
+   public char getLetter ()
+   {
+      return letter;
+   }
+
+   /**
+    * @param letter the letter to set
+    */
+   public void setLetter (char ltr)
+   {
+      letter = ltr;
+   }
+
 }
